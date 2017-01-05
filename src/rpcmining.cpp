8c88aeec25d44ef05baee7368096ff5ec725736b
hihiben <hihiben@gmail.com>
2017-01-05 16:52:52 +0800

    fix rpcmining.cpp
    getblocktemplate()

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -571,7 +571,7 @@ Value getblocktemplate(const Array& params, bool fHelp)
     result.push_back(Pair("previousblockhash", pblock->hashPrevBlock.GetHex()));
     result.push_back(Pair("transactions", transactions));
     result.push_back(Pair("coinbaseaux", aux));
-    result.push_back(Pair("coinbasevalue", (int64_t)pblock->vtx[0].vout[0].nValue));
+    result.push_back(Pair("coinbasevalue", ValueFromAmount(pblock->vtx[0].vout[0].mValue)));
     result.push_back(Pair("longpollid", chainActive.Tip()->GetBlockHash().GetHex() + i64tostr(nTransactionsUpdatedLast)));
     result.push_back(Pair("target", hashTarget.GetHex()));
     result.push_back(Pair("mintime", (int64_t)pindexPrev->GetMedianTimePast()+1));
